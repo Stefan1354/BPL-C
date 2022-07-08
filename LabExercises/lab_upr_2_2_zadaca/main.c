bcbb3c167ec32db80563ac341dc7173fce90305b
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:34:40 +0300

    Update main.c

--- LabExercises/lab_upr_2_2_zadaca/main.c
+++ LabExercises/lab_upr_2_2_zadaca/main.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 
-//2.Da se napise programa na C, koqto na 2 stoinosti, vavedeni ot klaviaturata, shte razmeni mestata.
+//2. Write a program that will exchange the places of 2 values entered from the keyboard
+
 int main()
 {
-    int a,b;
+    int a, b;
     printf("Enter a and b: ");
-    scanf("%d %d", &a,&b);
+    scanf("%d %d", &a, &b);
 
     //a=5, b=3
-    a+=b; //a=8 b=3
-    b=a-b; //a=8 b=5
-    a-=b;  //a=3 b=5
-    printf("a=%d,b=%d",a,b);
+    a += b; //a=8 b=3
+    b = a - b; //a=8 b=5
+    a -= b;  //a=3 b=5
+    printf("a = %d,b = %d", a, b);
     return 0;
 }
