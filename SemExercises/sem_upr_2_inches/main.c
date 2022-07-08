89a7a4ccd2657aad7956b93f02ab71d4d337cbb3
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:23:26 +0300

    Update main.c

--- SemExercises/sem_upr_2_inches/main.c
+++ SemExercises/sem_upr_2_inches/main.c
@@ -5,16 +5,16 @@
 
 int main()
 {
-   float inch,mm,cm,dm,m;
-   printf("Convert inch to mm,cm,dm and m\n\nType inch: ");
+   float inch, mm, cm, dm, m;
+   printf("Convert inch to mm, cm, dm and m\n\nType inch: ");
    scanf("%f", &inch);
-   mm=inch*25.4;
-   cm=inch*2.54;
-   dm=inch*0.254;
-   m=inch*0.0254;
-   printf("\nmm: %f",mm);
-   printf("\ncm: %f",cm);
-   printf("\ndm: %f",dm);
-   printf("\nm: %f",m);
+   mm = inch * 25.4;
+   cm= inch * 2.54;
+   dm = inch * 0.254;
+   m = inch * 0.0254;
+   printf("\nmm: %f", mm);
+   printf("\ncm: %f", cm);
+   printf("\ndm: %f", dm);
+   printf("\nm: %f", m);
    return 0;
 }
