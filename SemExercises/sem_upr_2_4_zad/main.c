1558bb37bafe6353e0979aff6360c9b908a00166
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:19:32 +0300

    Update main.c

--- SemExercises/sem_upr_2_4_zad/main.c
+++ SemExercises/sem_upr_2_4_zad/main.c
@@ -10,17 +10,17 @@ char c;
     scanf("%c", &c);
     printf("\n");
 
-    for(int i = 1; i <= 5; i++)
+    for(int i=1; i<=5; i++)
     {
-        for(int space = 0; space <= 5 - i; space++)
+        for(int space=0; space<=5-i; space++)
         {
             printf("   ");
         }
 
-        for(int j = 1; j <= i * 2 - 1; j++)
+        for(int j=1; j<=i*2-1; j++)
         {
 
-            if (j == 1 || (j == i * 2 - 1) || i == 5 )
+            if (j==1 || (j==i*2-1) || i==5 )
             {
                 printf(" %c ", c);
             }
