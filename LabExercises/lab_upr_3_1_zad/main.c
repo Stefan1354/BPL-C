531238f1c1afdba3c8472519ab7a42a198368607
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:38:02 +0300

    Update main.c

--- LabExercises/lab_upr_3_1_zad/main.c
+++ LabExercises/lab_upr_3_1_zad/main.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 
 //Да се прочетат от клавиатурата 3 числа и да се изведе на екрана най-малкото от тях.
+
 //1 nacin
+
 int main()
 {
-    int a,b,c,min;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    scanf("%d",&c);
+    int a, b, c, min;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
 
 
-    min=a;
+    min = a;
     if (b<min){
-        min=b;
+        min = b;
     }
     if(c<min){
-        min=c;
+        min = c;
     }
 
-    printf("%d is the smallest number ",min);
+    printf("%d is the smallest number ", min);
 
     return 0;
 }
@@ -27,25 +29,22 @@ int main()
 
 //2 nacin
 
-#include <stdio.h>
-#include <stdlib.h>
-
-//int main()
-//{
-//    int a, b, c;
-//    scanf("%d", &a);
-//    scanf("%d", &b);
-//    scanf("%d", &c);
-//    if (a<b && a<c) {
-//        printf("%d is min", a);
-//    }
-//    else if (b<a && b<c) {
-//        printf("%d is min", b);
-//    }
-//    else if (c<a && c<b) {
-//        printf("%d is min", c);
-//    }
-//    else
-//        printf("No min number");
-//    return 0;
-//}
+int main()
+{
+   int a, b, c;
+   scanf("%d", &a);
+   scanf("%d", &b);
+   scanf("%d", &c);
+   if (a<b && a<c) {
+       printf("%d is min", a);
+   }
+   else if (b<a && b<c) {
+       printf("%d is min", b);
+   }
+   else if (c<a && c<b) {
+       printf("%d is min", c);
+   }
+   else
+       printf("No min number");
+   return 0;
+}
