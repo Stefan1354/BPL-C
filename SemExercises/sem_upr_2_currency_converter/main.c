e29544c54f8cb53bb41d30145ff844d844e0ef65
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:22:21 +0300

    Update main.c

--- SemExercises/sem_upr_2_currency_converter/main.c
+++ SemExercises/sem_upr_2_currency_converter/main.c
@@ -13,14 +13,14 @@ int main()
     printf("Enter lev: ");
     scanf("%f", &lev);
 
-    euro=lev*0.31;
-    printf("Lev in evro is %.2f: \n",euro);
+    euro = lev * 0.31;
+    printf("Lev in evro is %.2f: \n", euro);
 
-    dolar=lev*0.58;
-    printf("Lev in dollar is %.2f: \n",dollar);
+    dolar= lev * 0.58;
+    printf("Lev in dollar is %.2f: \n", dollar);
 
-    pound=lev*0.43;
-    printf("Lev in pound is %.2f: \n",pound);
+    pound = lev * 0.43;
+    printf("Lev in pound is %.2f: \n", pound);
 
 
     return 0;
