f4b63e94bca51f95a13f39ed95c9c1a1cbcb69f6
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:24:39 +0300

    Update main.c

--- SemExercises/sem_upr_2_temperature_converter/main.c
+++ SemExercises/sem_upr_2_temperature_converter/main.c
@@ -8,7 +8,7 @@ int main()
    float celsius,fahrenheit;
    printf("Enter temperature in celsius: ");
    scanf("%f", &celsius);
-   fahrenheit=(celsius*1.8)+32;
-   printf("%.2f Celsius = %.2f Fahrenheit",celsius,fahrenheit);
-return 0;
+   fahrenheit = (celsius*1.8) + 32;
+   printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
+   return 0;
 }
