fbd2094a5977ec19e589db34de94b89fcfa08db3
Stefan Stoilkov <101529092+Stefan1354@users.noreply.github.com>
2022-07-09 00:51:26 +0300

    Update main.c

--- LabExercises/lab_upr_3_7_zad/main.c
+++ LabExercises/lab_upr_3_7_zad/main.c
@@ -8,25 +8,22 @@ int main()
 {
     int n;
     printf("Enter N: ");
-	scanf("%d", &n);
-	int k;
+    scanf("%d", &n);
+    int k;
     printf("Enter K: ");
-	scanf("%d", &k);
-
-	int new_num;
-
-	int count = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-        printf("Enter N numbers: ");
-		scanf("%d", &new_num);
-		if (new_num < k && new_num % 3 != 0)
-		{
-			count++;
-		}
-	}
-	printf("\nNumber of numbers which are smaller than K and are not divisible by 3 is %d.", count);
-
+    scanf("%d", &k);
+    int new_num;
+    int count = 0;
+	
+    for (int i=0; i<n; i++)
+    {
+         printf("Enter N numbers: ");
+	 scanf("%d", &new_num);
+	 if (new_num < k && new_num % 3 != 0)
+	 {
+		count++;
+	 }
+     }
+     printf("\nNumber of numbers which are smaller than K and are not divisible by 3 is %d.", count);
     return 0;
 }
