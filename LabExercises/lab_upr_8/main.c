ee8c66922f9fae530035a2e94c5eca2c6f0ffde2
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-07-23 12:59:21 +0300

    Update main.c

--- LabExercises/lab_upr_8/main.c
+++ LabExercises/lab_upr_8/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//1.Napisete programa na C, koqto namira sumata na N elementa vavedeni ot potrebitelq.Za izpulnenieto na programata zadelete pametta
-//dinamicno izpolzvaiki funkciite malloc ili calloc.
+//1.Napishete programa na C, koqto namira sumata na N elementa vavedeni ot potrebitelq.Za izpulnenieto na programata zadelete pametta
+//dinamichno izpolzvaiki funkciite malloc ili calloc.
 
 
 int main()
 {
-    int num,i,*ptr,sum=0;
+    int num, i, *ptr, sum=0;
     printf("Enter total number of elements: ");
-    scanf("%d",&num);
+    scanf("%d", &num);
     ptr=(int*)malloc(num*sizeof(int));
 
     if(ptr==NULL)
@@ -20,12 +20,12 @@ int main()
 
     printf("Enter elements: \n");
 
-    for(i=0;i<num;i++)
+    for(i=0; i<num; i++)
     {
-        scanf("%d",ptr+i);
+        scanf("%d", ptr+i);
         sum+=*(ptr+i);
     }
-    printf("Sum=%d",sum);
+    printf("Sum=%d", sum);
     free(ptr);
 
     return 0;
