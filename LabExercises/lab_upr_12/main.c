ca2ed3f61e86868272199e1ca5b57876d7f515e0
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-07-30 15:23:30 +0300

    Update main.c

--- LabExercises/lab_upr_12/main.c
+++ LabExercises/lab_upr_12/main.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 
 //1. Demonstraciq na rabota na funkciite fprintf i fscanf.
-//Da se napishe programa s izpolzvane na funkciq fprintf, zapisvat se edno cqlo chislo, drobno chislo i edin niz.
+//Da se napishe programa s izpolzvane na funkciq fprintf. Zapisvat se edno cqlo chislo, drobno chislo i edin niz.
 //Zatvarqme faila otvarqme go v rejim na chetene i otvarqme faila i cheteme zapisanoto s fscanf.
 
-//int main()
-//{
-//    int a = 5;
-//    double b = 4.3;
-//    char c[10] = "hello";
-//
-//    FILE *fp;
-//    fp = fopen("myfile", "w");
-//    if (fp == NULL)
-//    {
-//        printf("Error opening file");
-//        exit(1);
-//    }
-//    fprintf(fp, "%d %.2lf %s", a, b, c);
-//    fclose(fp);
-//
-//    fp = fopen("myfile", "r");
-//    if (fp == NULL)
-//    {
-//        printf("Error opening file");
-//        exit(1);
-//    }
-//    fscanf(fp, "%d %lf %s", &a, &b, c);
-//    fclose(fp);
-//    return 0;
-//}
+int main()
+{
+   int a = 5;
+   double b = 4.3;
+   char c[10] = "hello";
+
+   FILE *fp;
+   fp = fopen("myfile", "w");
+   if (fp == NULL)
+   {
+       printf("Error opening file");
+       exit(1);
+   }
+   fprintf(fp, "%d %.2lf %s", a, b, c);
+   fclose(fp);
+
+   fp = fopen("myfile", "r");
+   if (fp == NULL)
+   {
+       printf("Error opening file");
+       exit(1);
+   }
+   fscanf(fp, "%d %lf %s", &a, &b, c);
+   fclose(fp);
+   return 0;
+}
 
 //2.Imame masiv s 10 elementa(drobni chisla). Inicializirame masiva s nqkakvi stoinosti, zapisvame vuv fail masiva,
 //izpolzvaiki funkciqta fwrite, sled koeto potrebitelqt vavejda koi element ot toji masiv da se prochete i go printirame na ekrana.
@@ -40,7 +40,7 @@
 int main()
 {
     float array[SIZE];
-    for (int i = 0; i < SIZE; i++)
+    for (int i=0; i<SIZE; i++)
     {
         printf("Enter %d. element: ", i + 1);
         scanf("%f", &array[i]);
@@ -76,5 +76,3 @@ int main()
 
     return 0;
 }
-
-
